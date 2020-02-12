8dbb25b735e132ba9eeb4b45595ca630ce833279
metamn <bartus.csongor@gmail.com>
2020-02-12 08:39:00 +0200

    Ready for use-api

--- src/components/FinsterInfo/FinsterInfo.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import FinsterInfo from "./FinsterInfo";
-
-it("has a FinsterInfo component", () => {
-  const { getByText } = render(<FinsterInfo />);
-  expect(getByText("FinsterInfo")).toBeInTheDocument();
-});
